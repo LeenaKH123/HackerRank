2b21d9c20708cee1152df7fa1bf19b91617ae85a
leenakhalili <engleena@yahoo.com>
2021-11-29 11:06:38 +0300

    List comprehension

--- BasicDataTypes/ListComprehension.py
+++ BasicDataTypes/ListComprehension.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     for i in range(x+1):
         for j in range(y+1):
             for k in range(z+1):
-                if(i+j+k != 2):
+                if(i+j+k != n):
                     l.append([i,j,k])
     print(l)
