349ea855868289e95a0953e08e389f0da475c8a1
leenakhalili <engleena@yahoo.com>
2021-11-29 14:32:13 +0300

    List

--- BasicDataTypes/Lists.py
+++ BasicDataTypes/Lists.py
@@ -5,7 +5,21 @@ if __name__ == '__main__':
     for i in range(N):
         cmd = input().split() # insert 10 5. ['insert', 10, 5]
         if cmd[0]=='insert':
-            lst.insert(int(cmd[1]),int(cmd[2]))
-        if cmd[0]=='print':
-            print(cmd(1), cmd(2))
+            lst.insert(int(cmd[1]),int(cmd[2]))         
+        elif cmd[0]=='print':
+            print(lst)
+        elif cmd[0]=='remove':
+            lst.remove(int(cmd[1]))
+        elif cmd[0]=='append':
+            lst.append(int(cmd[1]))
+        elif cmd[0]=='sort':
+            lst.sort()
+        elif cmd[0]=='pop':
+            lst.pop()
+        else:
+            lst.reverse()
+        
+        
+
+    
         
