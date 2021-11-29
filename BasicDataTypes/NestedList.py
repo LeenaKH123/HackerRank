26423b7540bc49ce2b3e41bdbf6563ae7d859f22
leenakhalili <engleena@yahoo.com>
2021-11-29 23:00:25 +0300

    Nested List

---
+++ BasicDataTypes/NestedList.py
@@ -0,0 +1,7 @@
+n = int(input()) # get the number of students from the user
+res = [] # initialise the list
+for i in range(n):  
+    name = input() #get the name of the student            # for loop to get all the student values
+    mark = int(input()) # get the mark of the student
+    
+
